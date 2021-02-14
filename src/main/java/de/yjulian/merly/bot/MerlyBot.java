09f6dc1b915867bba8304ce27d3f6b704e424c79
yJulian <yJulian@outlook.de>
2021-02-14 20:54:59 +0100

    Documented MerlyBot.java

--- src/main/java/de/yjulian/merly/bot/MerlyBot.java
+++ src/main/java/de/yjulian/merly/bot/MerlyBot.java
@@ -31,6 +31,12 @@ public class MerlyBot {
     private AudioManager audioManager;
     private ConsoleManager consoleManager;
 
+    /**
+     * Create a new instance from the MerlyBot
+     *
+     * @param token the discord bot token.
+     * @throws Exception an general exception.
+     */
     public MerlyBot(String token) throws Exception {
         instance = this;
 
@@ -44,17 +50,28 @@ public class MerlyBot {
         setProgramState(ProgramState.RUNNING);
     }
 
+    /**
+     * Method to shutdown the bot with all of the components.
+     */
     public void shutdown() {
         LOGGER.info("Shutting down.");
         setProgramState(ProgramState.SHUTDOWN);
         this.jda.shutdown();
     }
 
+    /**
+     * Method to set the new program state {@link ProgramState}
+     *
+     * @param newState the new state. e.g. PostInit, Init, PreInit
+     */
     private void setProgramState(ProgramState newState) {
         this.eventManager.fireEvent(new ProgramStateChangedEvent(currentProgramState, newState));
         this.currentProgramState = newState;
     }
 
+    /**
+     * Method to handle the pre initialization.
+     */
     private void preInit() {
         setProgramState(ProgramState.PRE_INIT);
 
@@ -65,12 +82,20 @@ public class MerlyBot {
         this.eventManager.addEventAdapter(this.moduleManager, this.consoleManager, this.commandManager);
     }
 
+    /**
+     * Method to handle the initialization.
+     */
     private void init() {
         setProgramState(ProgramState.INIT);
 
         this.audioManager = new AudioManager();
     }
 
+    /**
+     * Method to handle the post initialization.
+     *
+     * @throws Exception an exception.
+     */
     private void postInit() throws Exception {
         setProgramState(ProgramState.POST_INIT);
 
@@ -84,34 +109,72 @@ public class MerlyBot {
         );
     }
 
+    /**
+     * Get the command manager to register new commands or get the registered ones.
+     *
+     * @return a {@link CommandManager} initialized for the current instance.
+     */
     public static CommandManager getCommandManager() {
         return getInstance().commandManager;
     }
 
+    /**
+     * Get the current instance from the bot.
+     *
+     * @return the active instance.
+     */
     public static MerlyBot getInstance() {
         return instance;
     }
 
+    /**
+     * Get the console manager.
+     *
+     * @return a {@link ConsoleManager}
+     */
     public ConsoleManager getConsoleManager() {
         return consoleManager;
     }
 
+    /**
+     * Get the module Manager
+     * @return a {@link ModuleManager}
+     */
     public ModuleManager getModuleManager() {
         return moduleManager;
     }
 
+    /**
+     * Get the event Manager to call custom events or register new {@link de.yjulian.merly.events.EventAdapter}
+     * @return the {@link EventManager}
+     */
     public EventManager getEventManager() {
         return eventManager;
     }
 
+    /**
+     * Get the audio manager used to send audio to the bot instance.
+     *
+     * @return the {@link AudioManager}
+     */
     public AudioManager getAudioManager() {
         return audioManager;
     }
 
+    /**
+     * Get the active JDA
+     *
+     * @return the jda.
+     */
     public JDA getJDA() {
         return jda;
     }
 
+    /**
+     * Get the logger.
+     *
+     * @return a slj4j logger.
+     */
     public static Logger getLogger() {
         return LOGGER;
     }
